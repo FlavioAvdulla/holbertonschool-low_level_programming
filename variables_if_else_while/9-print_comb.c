a588fa4124b6f47d730e627d7e7bbdcb220c1db8
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-18 13:06:51 +0000

    changes

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/* more headers goes there *//* betty style doc for function main goes there */
 /**
 * main - Entry point
 *
@@ -10,17 +9,14 @@
 */
 int main(void)
 {
-int i;
-
-for (i = '0'; i <= '9'; i++)
+int i;for (i = '0'; i <= '9'; i++)
 {
 putchar(i);
-}
 if (i != '9')
 {
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
-return (0);
+}
+putchar('\n');return (0);
 }
