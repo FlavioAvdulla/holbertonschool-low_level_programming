eb418547e1091d1524ed38fd0a98a2059ee6f326
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-18 11:43:27 +0000

    update

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there *//* betty style doc for function main goes there */
+/* more headers goes there */
+/* betty style doc for function main goes there */
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
 int main(void)
-{char c;for (c = 'a'; c <= 'z'; c++)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
 {
 putchar(c);
 }
