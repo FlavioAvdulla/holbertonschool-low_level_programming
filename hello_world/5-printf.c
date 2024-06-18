94a08050c1d5fc7557f84770fed3aa73396e85ef
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-18 07:15:29 +0000

    update

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,15 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: Prints a specified string with proper grammar.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/* Print the specified string using printf function */
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	/* Return 0 to indicate successful execution */
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
