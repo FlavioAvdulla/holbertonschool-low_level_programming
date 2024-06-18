cf92d3f3ca97eac34730a2bdfa92fb6fad3b0fa6
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-18 10:45:08 +0000

    changes

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,18 +15,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    
+
     if (n > 0)
     {
         /* code */
     printf("%d is positive\n", n);
     }
-
     if (n == 0)
     {
     printf("%d is zero\n", n);
     }
-    
     if (n < 0)
     {
     printf("%d is negative\n", n);
