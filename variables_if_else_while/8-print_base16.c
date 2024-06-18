8b89fc895e4c6781189315b7c28e7b500b3264ce
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-18 12:28:59 +0000

    changes

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+int i;
+
+for (i = '0'; i < '9'; i++)
+{
+putchar(i);
+}
+for (i = 'a'; c < 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
