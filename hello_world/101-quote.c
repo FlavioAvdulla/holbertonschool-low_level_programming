3550626fb270faf4847beff14067e88ef843c351
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-19 10:11:01 +0000

    change

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    const char *message = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    
-    return(1);
-    
+const char *message = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return(1);
+
 }
