95414a908eb0be1bdcc4c5eb55adb3bf0cc3bb83
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-20 10:07:47 +0000

    changes

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,9 +3,7 @@
 /**
  * print_s Write a function that prints the sign of a number.
  * @c: char passed as argument
- * Returns 1 and prints + if n is greater than zero
- * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
+ * Returns 1 and prints + if n is greater than zero, Returns 0 and prints 0 if n is zero, Returns -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
