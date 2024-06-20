8bcce2d381956fb62b99e6dc5800256c21c6cb21
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-20 08:41:15 +0000

    changes

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - checks if
- *
- * Return: Always 0.
- */
+* _islower - checks if
+*
+* Return: Always 0.
+*/
 int _islower(int c)
 {
-	if ((c >= 'a') && (c <= 'z'))
-		return (1);
-	else
-		return (0);
+if ((c >= 'a') && (c <= 'z'))
+return (1);
+else
+return (0);
 }
