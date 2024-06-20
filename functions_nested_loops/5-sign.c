4429d86208daa4fb91fa250432d335b8e2decef8
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-20 10:11:23 +0000

    changes

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_s - print the sign of a number.
+ * print_sign - print the sign of a number.
  * @c: char passed as argument
  * Returns 1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
