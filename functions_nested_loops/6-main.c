2848fa19d4186032235b967ef0601b1045ce55ed
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-20 10:44:13 +0000

    changes

---
+++ functions_nested_loops/6-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Write a function that prints the last digit of a number.
+ * @n: the number to check
+ *Returns the value of the last digit
+ */
+int _abs(int)
+{
+
+	if(n > 0)
+		{
+		return (n);
+		}
+	
+	if(n < 0)
+		{
+		return (-n);
+		}
+
+}
