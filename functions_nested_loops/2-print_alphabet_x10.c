192f99db21fa3feb6dbe8f7a2a32343cefc8787c
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-20 08:19:00 +0000

    changes

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,8 @@ void print_alphabet_x10(void)
 	char letter;
 		for(i = 0; i < 10; i++)
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
 }
