74ce65b6d849d4075ae75503d4e1bcff5970e2a8
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-20 12:28:28 +0000

    change

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,11 +2,21 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- *
- * Return: void
  */
-int main(void)
+void jack_bauer(void)
 {
-    jack_bauer();
-    return (0);
+    int hour, min;
+    for(hour = 0; hour <= 23; hour++)
+    {
+        for(min = 0; hour <= 59; min++)
+        {
+            _putchar(hour / 10 + '0');
+            _putchar(hour % 10 + '0');
+            _putchar(':');
+            _putchar(min / 10 + '0');
+            _putchar(min % 10 + '0');
+            _putchar('\n');
+        }
+    
+    }
 }
