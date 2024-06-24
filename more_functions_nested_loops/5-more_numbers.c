b0574564c3b61d83c2ed8cfd4d954b71b37e724d
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-24 10:37:35 +0000

    changes

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@ void more_numbers(void)
 {
 
 	int i, j;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
@@ -20,6 +20,6 @@ void more_numbers(void)
 			_putchar ('0' + j % 10);
 		}
 		_putchar('\n');
-		
+
 	}
 }
