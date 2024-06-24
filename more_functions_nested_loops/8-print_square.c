47a9c498e4d3120d2f74d9e3a39633ae26a90217
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-24 12:28:08 +0000

    flavio

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,14 +11,22 @@
 
 void print_square(int size)
 
+void print_square(int size)
 {
+    int i, j;
 
-	int row, column;
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
 
-	for (row = 0; row < size; row++)
-	{
-		for (column = 0; column < size; column++)
-			_putchar ('#');
-		_putchar ('\n');
-	}
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
 }
