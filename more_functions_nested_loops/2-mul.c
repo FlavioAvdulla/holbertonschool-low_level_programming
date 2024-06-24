29bd6602a4aceba7fb6b2daf3675db99dc46b557
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-24 08:53:52 +0000

    changes

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,1 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - checks if a character is uppercase.
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int mul(int a, int b)
+
+{
+	if (a >= '0' && b <= '9')
+		return (1);
+	else
+		return (0);
+}
