2f24d13cc6ed2961ff1c8bbc1150bfabb7990897
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-24 12:58:09 +0000

    changes

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,27 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle followed by a new line.
+ * @size: Size of the triangle.
+ */
+void print_triangle(int size)
+{
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (int row = 1; row <= size; row++)
+    {
+        for (int space = size - row; space > 0; space--)
+        {
+            _putchar(' ');
+        }
+        for (int hash = 1; hash <= row; hash++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+}
