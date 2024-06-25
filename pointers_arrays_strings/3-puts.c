a178bbb828f4a5a2c38e3dfffd0f53c742bd547f
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-25 08:44:58 +0000

    string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,14 @@
 #include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: Pointer to the string to be printed.
+ */
+void _puts(char *str)
+
+{
+	char greeting[] = {'H', 'e', 'l', 'o', '\0'};
+	printf ("Greetings message: %s\n", greetings);
+	
+	return (0);
+}
