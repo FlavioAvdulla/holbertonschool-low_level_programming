52cacdd61f2625b6b9f482089d7cd9f4a77c32d1
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-25 09:58:11 +0000

    changes

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@ void rev_string(char *s)
 {
 	int length = 0;
 	char *start, *end, temp;
-	while (*(s + length) != '\0')
+	while (* (s + length) != '\0')
 	length++;
 	start = s;
 	end = s + length - 1;
