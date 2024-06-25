08cfad4f6a1e5ef92a2839b97c84dc22d7e9759c
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-25 09:54:40 +0000

    changes

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,14 @@
  * @s: Pointer to the string to be reversed.
  */
 void rev_string(char *s)
-
 {
 	int length = 0;
 	char *start, *end, temp;
 
 	while (*(s + length) != '\0')
 	length++;
-
 	start = s;
-	end = s + length -1;
+	end = s + length - 1;
 
 	while (start < end)
 	{
