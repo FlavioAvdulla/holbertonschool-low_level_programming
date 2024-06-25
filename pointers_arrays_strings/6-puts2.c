0d40f56d7a6a3b6aadf2df4e91df0e67a9b73352
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-25 13:31:36 +0000

    changes

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 
 /**
 * puts2 - Prints every other character of a string, starting with the first character,
-*         followed by a new line.
+* followed by a new line.
 * @str: The input string.
 */
 void puts2(char *str)
