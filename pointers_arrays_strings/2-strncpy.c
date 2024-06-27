c2bd6e4c7c6266b1d6a1c95c28efbbf09722732b
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-27 10:46:49 +0000

    changes

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,19 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
-* _strncpy - Copies at most n characters from the string pointed to by src,
-* including the null terminator, to the array pointed to by dest.
-* If the length of src is less than n, null characters are
-* appended until the total of n characters have been copied.
-* @dest: A pointer to the destination array where the content is to be copied.
-* @src: A pointer to the source of data to be copied.
-* @n: The maximum number of characters to be copied from src.
-*
-* Return: A pointer to the destination string dest.
-*/
-char *_strncpy(char *dest, char *src, int n)
+ * _strncpy - Copies a string.
+ * @dest: The destination string buffer.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copy from src.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strncpy(char *dest,  char *src, int n)
 {
-int i;
-
-for (i = 0; i < n && src[i] != '\0'; i++)
+char *copy = dest;    while (*src && n > 0){
+        *dest++ = *src++;
+        n--;}    while (n > 0)
 {
-	dest[i] = src[i];
-}
-for (; i < n; i++)
-{
-	dest[i] = '\0'; // Null-padding if n > length of src
-}
-
-return dest;
+        *dest++ = '\0';
+        n--;
+}    return (copy);
 }
--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,38 @@
+Write a function that compares two strings.
+
+    Prototype: int _strcmp(char *s1, char *s2);
+    Your function should work exactly like strcmp
+
+FYI: The standard library provides a similar function: strcmp. Run man strcmp to learn more.
+
+julien@ubuntu:~/$ cat 3-main.c
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _puts - Prints a string followed by a new line to stdout.
- * @str: Pointer to the string to be printed.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void _puts(char *str)
-
+int main(void)
 {
-	while (*str != '\0')
-	{
-		_putchar (*str);
-		str++;
-	}
-	_putchar('\n');
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
+julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strcmp.c -o 3-strcmp
+julien@ubuntu:~/$ ./3-strcmp 
+-15
+15
+0
+julien@ubuntu:~/$ 
+
+Repo:
+
+    GitHub repository: holbertonschool-low_level_programming
+    Directory: pointers_arrays_strings
+    File: 3-strcmp.c
