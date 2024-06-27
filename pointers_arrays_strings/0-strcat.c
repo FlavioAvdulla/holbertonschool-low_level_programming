931adf4593567f47d675429f3f45ac66de69ca9e
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-27 09:05:08 +0000

    changes

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -15,15 +15,15 @@ int i, j;
 i = 0;
 while (dest[i] != '\0')
 {
-    i++;
+	i++;
 }
 
 j = 0;
 while (src[j] != '\0')
 {
-    dest[i] = src[j];
-    i++;
-    j++;
+	dest[i] = src[j];
+	i++;
+	j++;
 }
 dest[i] = '\0';
 
