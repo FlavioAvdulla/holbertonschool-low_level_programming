c794a5d722c4a502a7fbe5767bb06ed54b28bc30
Flavio Avdulla <a.flavio4366@gmail.com>
2024-06-27 10:58:58 +0000

    changes

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,12 @@
 * respectively, to be less than, to match, or be greater than s2.
 */
 int _strcmp(char *s1, char *s2);
+
 {
 while (*s1 && (*s1 == *s2))
 {
 	s1++;
 	s2++;
 }
-return *(unsigned char *)s1 - *(unsigned char *)s2;
+return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
