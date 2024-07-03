9a4b9bfc143987a7eb0466416231ae20a874fa43
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-03 19:20:27 +0000

    changes

--- recursion/file4.c
+++ recursion/file4.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 int main(void)
 {
-    int num1 = 5;
+    int num1 = 7;
     int num2 = 10;
     int result;
 
     result = add(num1, num2);
 
-    printf("%d\n", num1, num2, result);
-
+    printf("The sum of %d and %d is %d", num1, num2, result);
 
     return (0);
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
