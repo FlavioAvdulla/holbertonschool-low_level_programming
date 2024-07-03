199fa26de08cc70fddd51ecf9993b41be6182c0c
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-03 16:34:15 +0000

    changes

---
+++ recursion/file1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+    int number = 5;
+    float floating-point = 3.14;
+    char letter = 'A';
+    char string[] = 'im here';
+
+    printf("This is int number: %d\n", number);
+    printf("This is float number: %2f\n", floating_point);
+    printf("This is letter: %c\n", letter);
+    printf("This is string: %s\n", string);
+
+    
+    
+    return (0);
+}
