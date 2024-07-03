56de8dd325ca297d87075a806912b84b24158237
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-03 19:09:58 +0000

    changes

---
+++ recursion/file4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    int num1 = 5;
+    int num2 = 10;
+    int result;
+
+    result = add(num1, num2);
+
+    printf("%d\n", num1, num2, result);
+
+
+    return (0);
+}
