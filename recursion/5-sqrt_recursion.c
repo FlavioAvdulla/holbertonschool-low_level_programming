53ac43ab0c8205be1567cb9b0444209b8d975325
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-04 12:40:03 +0000

    changes

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@
 int _sqrt_helper(int n, int guess)
 {
 if (guess * guess == n)
-	return guess;
+	return (guess);
 if (guess * guess > n)
 	return (-1);
 return (_sqrt_helper(n, guess + 1));
@@ -25,7 +25,7 @@ return (_sqrt_helper(n, guess + 1));
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-	return -1;
+	return (-1);
 if (n == 0 || n == 1)
 	return (n);
 return (_sqrt_helper(n, 1));
