dbb3cfefca8758b8aded6ae6551c9166467b8bfd
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-05 09:04:41 +0000

    changes

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, result;
+
+	result = 0;
+	for (i = 0; i < argc; i++)
+	{
+		result += atoi(argv[i]);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
