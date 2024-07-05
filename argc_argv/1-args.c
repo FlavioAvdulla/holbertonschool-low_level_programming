f711be0fceeeed3b7c4bcddbfa30f531b7459483
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-05 08:15:07 +0000

    changes

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,7 +6,7 @@
  *
  * return: Always 0.
  */
-int main(int argc , char *argv[] __attribute__((__unused__)))
+int main(int argc, char *argv[] __attribute__((__unused__)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
