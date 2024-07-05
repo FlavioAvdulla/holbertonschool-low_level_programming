613d1b3b89c6395df767a4f42133b467a744102d
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-05 08:14:29 +0000

    changes

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * return: Always 0.
+ */
+int main(int argc , char *argv[] __attribute__((__unused__)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
