5253f1188521dae8e35b39f05c75cb09c669e574
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-08 07:26:25 +0000

    changes

--- recursion/file3.c
+++ recursion/file3.c
@@ -2,16 +2,18 @@
 
 int main()
 {
+    // Declare and initialize an array
     int numbers[] = {1, 3, 5, 7, 9};
-
+    // Determine the size of the array
     int size = sizeof(numbers) / sizeof(numbers[0]);
-
+    // Initialize a variable to hold the sum
     int sum = 0;
-
+    // Calculate the sum of all elements
     for (int i = 0; i < size; i++)
     {
         sum += numbers[i];
     }
+    // Print the sum
     printf("the sum of all elements of the array is: %d\n", sum);
 
     return (0);
