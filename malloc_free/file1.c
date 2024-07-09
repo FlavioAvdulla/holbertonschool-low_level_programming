a0c5fe15adef74863f8016fbdfc7959c638d763c
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-09 20:56:32 +0000

    changes

--- malloc_free/file1.c
+++ malloc_free/file1.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void print_int_array(int *a, int size)
 {
-int n;
-char *ar;
+    int i;
+    i = 0;
 
-n = 5;
-ar = malloc(n * sizeof(char));
-ar[0] = 'C';
-ar[1] = 'o';
-ar[2] = 'o';
-ar[3] = 'l';
-ar[4] = '\0';
+    while (i < size)
+    {
+        printf("%d\n", a[i]);
+        i++;
+    }
+}
+    
+int main(int ac, char **av)
+{
+    int *a;
+    int asize;
+    int i;
 
-printf("%s\n", ar);
+    if (ac < 2)
+    {
+        printf("Please give me at least one number\n");
+        printf("Usage: %s number [NUMBER]\n", av[0]);
+        return (1);
+    }
+
+    asize = ac - 1;
+    a = malloc(asize * sizeof(int));
+    i = 0;
+    while (i < asize)
+    {
+        a[i] = atoi(av[i + 1]);
+        i++;
+    }
+
+    print_int_array(a, asize);
+
 return (0);
 }
