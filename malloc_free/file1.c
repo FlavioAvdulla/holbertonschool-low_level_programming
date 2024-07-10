f2665743b68973bd2619cd4b17e1dc14d1d1d12b
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-10 21:11:20 +0000

    changes

--- malloc_free/file1.c
+++ malloc_free/file1.c
@@ -1,41 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_int_array(int *a, int size)
+char *str_concat(char *s1, char *s2)
 {
-    int i;
-    i = 0;
+	char *concat_str;
+	int i, j, len1 = 0, len2 = 0;
 
-    while (i < size)
-    {
-        printf("%d\n", a[i]);
-        i++;
-    }
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	concat_str = malloc((len1 + len2 +1) * sizeof(char));
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		concat_str[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+		concat_str[i + j] = s2[j];
+
+	concat_str[i + j] = '\0';
+
+	return (concat_str);
 }
-    
-int main(int ac, char **av)
-{
-    int *a;
-    int asize;
-    int i;
 
-    if (ac < 2)
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s;
+
+    s = str_concat("Best ", "School");
+    if (s == NULL)
     {
-        printf("Please give me at least one number\n");
-        printf("Usage: %s number [NUMBER]\n", av[0]);
+        printf("failed\n");
         return (1);
     }
-
-    asize = ac - 1;
-    a = malloc(asize * sizeof(int));
-    i = 0;
-    while (i < asize)
-    {
-        a[i] = atoi(av[i + 1]);
-        i++;
-    }
-
-    print_int_array(a, asize);
-
-return (0);
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
