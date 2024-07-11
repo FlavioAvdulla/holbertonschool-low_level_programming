1d00d5e9140030baf6d6c6f761f6e35062970a47
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-11 07:28:58 +0000

    changes

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* string_nconcat - Concatenates two strings.
+* @s1: The first string.
+* @s2: The second string.
+* @n: The number of bytes to include from s2.
+*
+* Return: A pointer to the newly allocated space in memory
+* containing s1 followed by the first n bytes of s2,
+* and null terminated. If the function fails, return NULL.
+* If n is greater or equal to the length of s2 then use the entire s2.
+* If NULL is passed for s1 or s2, treat it as an empty string.
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+	for (j = 0; j < n; j++)
+		concat[len1 + j] = s2[j];
+
+	concat[len1 + j] = '\0';
+
+	return (concat);
+}
