2a1763bcb9c5cbcf27687e865ef4b88a98551d05
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-16 12:26:40 +0000

    changes

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,12 +4,12 @@
  * print_name - Print a name from a function pointer
  * @name: char string
  * @f: function pointer that takes a string argument
- */
+*/
 
- void print_name(char *name, void (*f)(char *f))
- {
+void print_name(char *name, void (*f)(char *f))
+{
 	if (name != NULL && f != NULL)
 	{
 		f(name);
 	}
- }
+}
