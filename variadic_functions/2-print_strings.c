e63ca55eb5f483b57c45478bd1c7f2a969514812
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-19 07:18:37 +0000

    2-print_string.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -9,7 +9,7 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	unsigned i;
+	unsigned int i;
 	const char *str;
 
 	va_start(args, n);
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		if (separator != NULL && i < n -1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 
