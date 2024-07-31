c370bfaf57ef3c9e5c00643d36fd6afc40fa79ef
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-31 10:16:43 +0000

    changes

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -5,7 +5,6 @@
 * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
 * @head: Double pointer to the head of the list.
 * @n: Data to insert in the new node.
-* 
 * Return: The address of the new element, or NULL if it failed.
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
@@ -32,7 +31,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
 * @head: Double pointer to the head of the list.
 * @n: Data to insert in the new node.
-* 
 * Return: The address of the new element, or NULL if it failed.
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
