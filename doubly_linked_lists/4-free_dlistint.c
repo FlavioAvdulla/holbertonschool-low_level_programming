21f3bda31ea353df1350f29322776e9ea63c934a
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-31 10:33:05 +0000

    changes

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
-* print_dlistint - Prints all the elements of a dlistint_t list.
-* @h: Pointer to the head of the list.
-* Return: The number of nodes.
+* free_dlistint - Frees a dlistint_t list.
+* @head: Pointer to the head of the list.
 */
-size_t print_dlistint(const dlistint_t *h)
+void free_dlistint(dlistint_t *head)
 {
-	size_t count = 0;
+	dlistint_t *temp;
 
-	while (h != NULL)
+	while (head != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
+		temp = head;
+		head = head->next;
+		free (temp);
 	}
-
-	return (count);
 }
