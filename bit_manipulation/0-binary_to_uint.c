aec8e0dccd26695160eff22ac98321c88c72af15
Flavio Avdulla <a.flavio4366@gmail.com>
2024-08-01 10:14:49 +0000

    changes

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,29 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* binary_to_uint - Converts a binary number to an unsigned int.
-* @b: A pointer to a string of 0 and 1 chars.
-* Return: The converted number, or 0 if there is an invalid character
-* or if b is NULL.
-*/
+ * binary_to_uint - function that converst a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ * Return: the converted number or 0 if there is one or more chars in the
+ * string b that is not 0 or 1, b is NULL
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum = 0;
-	int i, len = 0;
+	unsigned int s = 0, w = 1;
+	int j, l = 0;
 
 	if (b == NULL)
 		return (0);
-
-	for (len = 0; b[len] != '\0'; len++);
-
-	for (i = len - 1; i >= 0; i--)
+	for (j = 0; b[j] != '\0'; j++)
+		l++;
+	for (j = (l - 1); j >= 0; j--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[j] != '0' && b[j] != '1')
 			return (0);
-
-		if (b[i] == '1')
-			sum += (1 << (len - i - 1));
+		if (b[j] == '1')
+			s = s + w;
+		w = w * 2;
 	}
-
-	return (sum);
+	return (s);
 }
