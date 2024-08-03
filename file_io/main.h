6b0427b93ae79fcfbd1f582c0b13d02a0d22cdcb
Flavio Avdulla <a.flavio4366@gmail.com>
2024-08-03 13:50:44 +0000

    changes

--- file_io/main.h
+++ file_io/main.h
@@ -1,19 +1,16 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unistd.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void cp(char *file_from, char *file_to);
+void check_fails(char *file_from, char *file_to);
+void closefd(int fd1, int fd2);
 
 #endif
