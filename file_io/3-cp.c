45db728c7d9dfab15576b9cc43564339d39c2c69
Flavio Avdulla <a.flavio4366@gmail.com>
2024-08-03 13:51:08 +0000

    changes

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,78 +1,78 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
 
 /**
- * _errexit - print error message and exit
- * @str: err message as string
- * @file: file name as string
- * @code: exit code
- * Return: void
+ * closefd - closes a file descriptor
+ * @fd: file descriptor
  */
-void _errexit(char *str, char *file, int code)
+void closefd(int fd)
 {
-	dprintf(STDERR_FILENO, str, file);
-	exit(code);
-}
-
-/**
- * _cp - copy source file to destination file
- * @file_from: source file
- * @file_to: destination file
- * Return: void
- */
-void _cp(char *file_from, char *file_to)
-{
-	int fd1, fd2, numread, numwrote;
-	char buffer[1024];
-
-	fd1 = open(file_from, O_RDONLY);
-	if (fd1 == -1)
-		_errexit("Error: Can't read from file %s\n", file_from, 98);
-
-	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd2 == -1)
-		_errexit("Error: Can't write to %s\n", file_to, 99);
-
-	numread = 1024;
-	while (numread == 1024)
+	if (close(fd) == -1)
 	{
-		numread = read(fd1, buffer, 1024);
-		if (numread == -1)
-			_errexit("Error: Can't read from file %s\n", file_from, 98);
-
-		numwrote = write(fd2, buffer, numread);
-
-		if (numwrote == -1)
-			_errexit("Error: Can't write to %s\n", file_to, 99);
-	}
-
-	if (numread == -1)
-		_errexit("Error: Can't read from file %s\n", file_from, 98);
-	if (close(fd2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
-	if (close(fd1) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
+
 /**
- *main - copies a file to another file
- *@argc: number of arguments passed to function
- *@argv: array containing arguments
- *Return: 0 on success
+ * main - Entry point
+ * @argc: number of arguments supplied to argv
+ * @argv: array of arguments
+ * Return: 0 on success, or an appropriate error code on failure
  */
 int main(int argc, char *argv[])
 {
+	int fdr, fdw, n, m;
+	char buffer[BUFFER_SIZE];
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	_cp(argv[1], argv[2]);
+	fdr = open(argv[1], O_RDONLY);
+	if (fdr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fdw = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fdw == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		closefd(fdr);
+		exit(99);
+	}
+
+	while ((n = read(fdr, buffer, BUFFER_SIZE)) > 0)
+	{
+		m = write(fdw, buffer, n);
+		if (m == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			closefd(fdr);
+			closefd(fdw);
+			exit(99);
+		}
+	}
+
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		closefd(fdr);
+		closefd(fdw);
+		exit(98);
+	}
+
+	closefd(fdr);
+	closefd(fdw);
 
 	return (0);
 }
